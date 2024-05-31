054044453160fcc36d8b137dde1f43ff0999b138
Forest Gregg <fgregg@datamade.us>
2024-05-31 10:22:44 -0400

    make a few more columns

--- scripts/make_donation.sql
+++ scripts/make_donation.sql
@@ -3,6 +3,17 @@ SELECT
     committees.name AS committee_name,
     strftime ('%Y-%m-%d', received_date) AS received_date,
     amount,
+    CASE WHEN d2_part = '1A' THEN
+        'individual contribution'
+    WHEN d2_part = '2A' THEN
+        'transfer'
+    WHEN d2_part = '3A' THEN
+        'loan'
+    WHEN d2_part = '4A' THEN
+        'other receipts'
+    WHEN d2_part = '5A' THEN
+        'in-kind'
+    END AS type,
     CASE WHEN first_name IS NOT NULL THEN
         first_name || ' ' || last_name
     ELSE
@@ -10,10 +21,13 @@ SELECT
     END AS name,
     occupation,
     employer,
-    receipts.city || ', ' || receipts.state || ' ' || receipts.zipcode AS city_state_zip
+    receipts.city || ', ' || receipts.state || ' ' || receipts.zipcode AS city_state_zip,
+    filed_doc_id,
+    doc_name
 FROM
     receipts
-    INNER JOIN committees ON committee_id = committees.id
+    INNER JOIN committees ON receipts.committee_id = committees.id
+    INNER JOIN filed_docs ON filed_doc_id = filed_docs.id
 WHERE
     received_date >= '2023-01-01'
-    AND archived = 0
+    AND receipts.archived = 0;
